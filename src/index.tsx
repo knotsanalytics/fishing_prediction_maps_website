cdda236de9b93b397db51c3c20723ac3e39edb37
Tommaso Laterza <tommasoltrz@gmail.com>
2022-03-24 10:46:09 +0100

    :sparkles: added PWA functionalities

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MapProvider } from "react-map-gl";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "@fontsource/ibm-plex-sans/400.css";
 import "@fontsource/ibm-plex-sans/500.css";
 import "@fontsource/ibm-plex-sans/700.css";
 import { theme } from "./styles/theme";
+
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
@@ -20,6 +22,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
